f2666768698295a4eb691a5c25ff1a1b1e085445
dcorral <hi@diegosw.com>
2020-02-12 10:40:13 +0100

    outlook and whatsapp web config

---
+++ outlook/outlook.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+chromium-browser --app=https://outlook.office365.com/mail --app-dir=/tmp
+
---
+++ whatsapp/whatsapp.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+chromium-browser --app=https://web.whatsapp.com --app-dir=/tmp
+
